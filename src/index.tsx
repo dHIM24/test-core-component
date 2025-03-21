5c02494341eccbd787dd0b208e9192043ad14007
Vadim Kalushko <vakalushko.job@yandex.ru>
2025-03-21 13:28:56 +0500

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      addressType='Адрес регистрации'
+      setStep={(step) => console.log('Step changed to:', step)}
+      setError={(error) => console.error('Error:', error)}
+      setConfirmationParams={(data) => console.log('Confirmation data:', data)}
+    />
   </React.StrictMode>,
 );
 
