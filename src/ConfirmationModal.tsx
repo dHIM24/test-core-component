24e383eefb60997198a9011517be4bcf52063ce7
Vadim Kalushko <vakalushko.job@yandex.ru>
2025-10-03 10:11:59 +0300

    ifx

--- src/ConfirmationModal.tsx
+++ src/ConfirmationModal.tsx
@@ -13,7 +13,7 @@ const ConfirmationModal: React.FC = () => {
     <div style={{ padding: '16px' }}>
       <button onClick={handleOpenModal}>Открыть модальное окно</button>
 
-      <UniversalModal open={isOpen} onClose={handleCloseModal} width={768}>
+      <UniversalModal open={isOpen} onClose={handleCloseModal} width={768} disableFocusLock={true}>
         <HeaderDesktop hasCloser={true} onClose={handleCloseModal} title='Подтверждение' />
         <div style={{ padding: '16px' }}>
           <ConfirmationBase />
